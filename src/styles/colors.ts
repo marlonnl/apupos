1adf4eb8d2c0cfc74a2e2a1a4da3a6dee6d93522
Marlonn Locatelli <marlonn@gmail.com>
2025-05-30 15:22:45 -0300

    add: main colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const color = {
+  bgPrimary: '#9AE4E8',
+  bgMenu: '#FFFFFF',
+  bgContent: '#FFFFFF',
+  bgSidebar: '#D1F9FA',
+  bgSidebarSecondary: '#E1FFFF',
+
+  text: '#37419A',
+  textSecondary: '#0084B4',
+  textComment: '#666666',
+  textReaminingChr: '#CCCCCC'
+}
