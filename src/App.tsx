cb4ab84574a4cd4e4d6a113d91505a5824e49f0f
Marlonn Locatelli <marlonn@gmail.com>
2025-05-30 16:51:35 -0300

    add: header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles'
 import Rotas from './routes'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <GlobalStyle />
+        <Header />
         <Rotas />
       </BrowserRouter>
     </>
