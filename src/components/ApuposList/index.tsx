5faa249efc13a1b732df00248d38b5b886c67f9a
Marlonn Locatelli <marlonn@gmail.com>
2025-05-31 16:24:17 -0300

    add: apupos list component

---
+++ src/components/ApuposList/index.tsx
@@ -0,0 +1,14 @@
+import Apupo from '../Apupo'
+
+const ApuposList = () => {
+  return (
+    <>
+      <Apupo></Apupo>
+      <Apupo></Apupo>
+      <Apupo></Apupo>
+      <Apupo></Apupo>
+    </>
+  )
+}
+
+export default ApuposList
