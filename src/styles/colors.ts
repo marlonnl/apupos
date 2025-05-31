40b469d58593815437bff952f98a6e7ffd881a94
Marlonn Locatelli <marlonn@gmail.com>
2025-05-31 16:23:57 -0300

    change: comment text color

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -7,7 +7,7 @@ export const color = {
 
   text: '#37419A',
   textSecondary: '#0084B4',
-  textComment: '#666666',
+  textComment: '#999999',
   textReaminingChr: '#CCCCCC',
 
   separtor: '#eeeeee'
