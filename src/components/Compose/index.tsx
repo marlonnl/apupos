5b9546d8e8cfb3f6eba4c285e945b2e646da1f4d
Marlonn Locatelli <marlonn@gmail.com>
2025-05-31 16:30:16 -0300

    add: compose component

---
+++ src/components/Compose/index.tsx
@@ -0,0 +1,18 @@
+import { ComposeContainer } from './styles'
+
+const Compose = () => {
+  return (
+    <ComposeContainer>
+      <div>
+        <h2>O que está acontecendo?</h2>
+        <span>140</span>
+      </div>
+      <textarea></textarea>
+      <div>
+        <button>apupar</button>
+      </div>
+    </ComposeContainer>
+  )
+}
+
+export default Compose
