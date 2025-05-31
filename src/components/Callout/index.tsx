96993abddae7bc25c4955b001c420c0a03ef6509
Marlonn Locatelli <marlonn@gmail.com>
2025-05-31 18:08:17 -0300

    add: callout componente created

---
+++ src/components/Callout/index.tsx
@@ -0,0 +1,16 @@
+import { CalloutBox, CalloutTitle } from './styles'
+
+const Callout = () => {
+  return (
+    <>
+      <CalloutBox>
+        <CalloutTitle>a•pu•po</CalloutTitle>
+        <p>(substantivo masculino)</p>
+        <p>1. alarido de troça; vaia;</p>
+        <p>2. buzina que produz som desabrido.</p>
+      </CalloutBox>
+    </>
+  )
+}
+
+export default Callout
---
+++ src/components/Callout/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import { color } from '../../styles/colors'
+
+export const CalloutBox = styled.div`
+  margin: 16px 0;
+  padding: 6px 12px;
+  font-size: 10px;
+
+  background-color: ${color.bgSidebarSecondary};
+  border-radius: 8px;
+`
+
+export const CalloutTitle = styled.p`
+  font-size: 12px;
+  color: ${color.textSecondary};
+  font-weight: bold;
+  margin-bottom: 4px;
+  letter-spacing: 2px;
+`
